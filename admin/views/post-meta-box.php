0b1665f6f8a31bc0a6c053d92384879604733224
iteras <iteras@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-07-15 11:07:38 +0000

    First public release
    git-svn-id: https://plugins.svn.wordpress.org/iteras/trunk@948729 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ admin/views/post-meta-box.php
@@ -0,0 +1,19 @@
+<?php
+
+wp_nonce_field( "post".$post->ID, 'iteras_paywall_post_nonce' );
+
+echo __('Who should be able to access this post?')."<br>";
+
+$i = 1;
+foreach ($this->access_levels as $level => $label) {
+
+  echo '<input id="iteras-paywall-radio'.$i.'" type="radio" name="iteras-paywall" value="'.$level.'" '. ($paywall_type == $level ? 'checked="checked"' : "").'>';
+  echo '<label for="iteras-paywall-radio'.$i.'" title="'.$level_descriptions[$level].'">'.$label.'</label><br>';
+
+  $i += 1;
+}
+
+if (!$settings['profile_name'] or !$settings['paywall_id'] or !$settings['subscribe_url'])
+  echo '<br><p class="description error">'.strtr(__("The ITERAS settings haven't been filled in properly. Go to the <a href='%url%'>settings page</a> to correct them."), array('%url%' => $settings_url)).'</p>';
+
+?>
---
+++ iteras.php
@@ -0,0 +1,53 @@
+<?php
+/**
+ * ITERAS
+ *
+ * @package   iteras
+ * @author    ITERAS Team <team@iteras.dk>
+ * @license   GPL-2.0+
+ * @link      http://www.iteras.dk
+ * @copyright 2014 ITERAS ApS
+ *
+ * @wordpress-plugin
+ * Plugin Name:       ITERAS
+ * Plugin URI:        http://app.iteras.dk
+ * Description:       Integration with ITERAS, a cloud-based state-of-the-art system for managing subscriptions and payments for magazines.
+ * Version:           0.2
+ * Author:            ITERAS
+ * Author URI:        http://www.iteras.dk
+ * Text Domain:       iteras
+ * License:           GPL-2.0+
+ * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
+ * Domain Path:       /languages
+ * WordPress-Plugin-Boilerplate: v2.6.1
+ */
+
+// If this file is called directly, abort.
+if ( ! defined( 'WPINC' ) ) {
+	die;
+}
+
+/*----------------------------------------------------------------------------*
+ * Public-Facing Functionality
+ *----------------------------------------------------------------------------*/
+
+require_once( plugin_dir_path( __FILE__ ) . 'includes/debug.php' );
+require_once( plugin_dir_path( __FILE__ ) . 'public/iteras-public.php' );
+
+register_activation_hook( __FILE__, array( 'Iteras', 'activate' ) );
+register_deactivation_hook( __FILE__, array( 'Iteras', 'deactivate' ) );
+register_uninstall_hook(__FILE__, array( 'Iteras', 'uninstall' ));
+
+add_action( 'plugins_loaded', array( 'Iteras', 'get_instance' ) );
+
+/*----------------------------------------------------------------------------*
+ * Dashboard and Administrative Functionality
+ *----------------------------------------------------------------------------*/
+
+// remove DOING_AJAX part to enable ajax
+if ( is_admin() && ( ! defined( 'DOING_AJAX' ) || ! DOING_AJAX ) ) {
+
+        require_once( plugin_dir_path( __FILE__ ) . 'admin/iteras-admin.php' );
+        add_action( 'plugins_loaded', array( 'Iteras_Admin', 'get_instance' ) );
+
+}
